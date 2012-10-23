b084bb30ccfac5c5b16a96a04c2c9f0b00d48441
Uri Goldshtein <uri.goldshtein@gmail.com>
2012-10-23 23:19:02 +0200

    Apply RSS

--- tree.js
+++ tree.js
@@ -39,7 +39,7 @@ function init(){
     var json = {
         id: "Life",
         name: "Life",
-        data: {'url':''},
+        data: {'url':'#/?urlParam=Map'},
         children: [{
             id: "Professionalism_and_education",
             name: "Professionalism and education",
